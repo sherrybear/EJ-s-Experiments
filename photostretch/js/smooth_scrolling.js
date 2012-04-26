6848d93233b22ec26461bca55c4e960c89adcfc9
EJ Fox <ejfox@ejfox.com>
2012-04-26 15:27:02 -0700

    Updating photostretch

--- photostretch/js/smooth_scrolling.js
+++ photostretch/js/smooth_scrolling.js
@@ -1,53 +1,57 @@
-var scrolloffset = 0;
-var scrolltiming = 1000;
+var scrollableElement, scrolloffset, scrolltiming;
 
-// Smooth scrolling
-$(document).ready(function() {
-  function filterPath(string) {
-  return string
-    .replace(/^\//,'')
-    .replace(/(index|default).[a-zA-Z]{3,4}$/,'')
-    .replace(/\/$/,'');
-  }
-  var locationPath = filterPath(location.pathname);
-  var scrollElem = scrollableElement('html', 'body');
+smoothScrolling = function(scroll) {
+    
+    scrolloffset = scroll || 0;
 
-  $('a[href*=#]').each(function() {
-    var thisPath = filterPath(this.pathname) || locationPath;
-    if (  locationPath == thisPath
-    && (location.hostname == this.hostname || !this.hostname)
-    && this.hash.replace(/#/,'') ) {
-      var $target = $(this.hash), target = this.hash;
+    scrolltiming = 1000;
+
+  var filterPath, locationPath, scrollElem;
+  filterPath = function(string) {
+    string.replace(/^\//, '').replace(/(index|default).[a-zA-Z]{3,4}$/, '').replace(/\/$/, '');
+    return string;
+  };
+  locationPath = filterPath(location.pathname);
+  scrollElem = scrollableElement('html', 'body');
+  return $('a[href*=#]').each(function() {
+    var $target, target, targetOffset, thisPath;
+    thisPath = filterPath(this.pathname) || locationPath;
+    if (locationPath === thisPath && (location.hostname === this.hostname || !this.hostname) && this.hash.replace(/#/, '')) {
+      $target = $(this.hash);
+      target = this.hash;
       if (target) {
-        var targetOffset = $target.offset().top-scrolloffset;
+          console.log("Target",$target);
+        targetOffset = $target.offset().top - scrolloffset;
         $(this).click(function(event) {
-          event.preventDefault();
-          $(scrollElem).animate({scrollTop: targetOffset}, scrolltiming, function() {
+//          console.log("Click", scrollElem);
+          $(scrollElem).animate({
+            scrollTop: targetOffset,
+            ease: 'easeOutQuart'
+          }, scrolltiming, function() {
             location.hash = target;
           });
+          event.preventDefault();
+          return false;
         });
       }
     }
   });
+};
 
-  // use the first element that is "scrollable"
-  function scrollableElement(els) {
-    for (var i = 0, argLength = arguments.length; i <argLength; i++) {
-      var el = arguments[i],
-          $scrollElement = $(el);
-      if ($scrollElement.scrollTop()> 0) {
-        return el;
-      } else {
-        $scrollElement.scrollTop(1);
-        var isScrollable = $scrollElement.scrollTop()> 0;
-        $scrollElement.scrollTop(0);
-        if (isScrollable) {
-          return el;
-        }
-      }
+scrollableElement = function(els) {
+  var $scrollElement, el, i, isScrollable, _i, _len;
+  for (_i = 0, _len = arguments.length; _i < _len; _i++) {
+    i = arguments[_i];
+    el = arguments[_i];
+    $scrollElement = $(el);
+    if ($scrollElement.scrollTop() > 0) {
+      return el;
+    } else {
+      $scrollElement.scrollTop(1);
+      isScrollable = $scrollElement.scrollTop() > 0;
+      $scrollElement.scrollTop(0);
+      if (isScrollable) return el;
     }
-    return [];
   }
-
-});
-
+  return [];
+};
